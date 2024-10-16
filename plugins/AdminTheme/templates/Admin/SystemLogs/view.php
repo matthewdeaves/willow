dd588db5285a8a989d4d4bd202b2a904197553ee
Matthew Deaves <mndeaves@me.com>
2024-10-16 23:26:11 +0100

    tidy up system log view

--- plugins/AdminTheme/templates/Admin/SystemLogs/view.php
+++ plugins/AdminTheme/templates/Admin/SystemLogs/view.php
@@ -1,51 +1,59 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\SystemLog $systemLog
- */
-?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit System Log'), ['action' => 'edit', $systemLog->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete System Log'), ['action' => 'delete', $systemLog->id], ['confirm' => __('Are you sure you want to delete # {0}?', $systemLog->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List System Logs'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New System Log'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="systemLogs view content">
-            <h3><?= h($systemLog->level) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= h($systemLog->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Level') ?></th>
-                    <td><?= h($systemLog->level) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Group Name') ?></th>
-                    <td><?= h($systemLog->group_name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($systemLog->created) ?></td>
-                </tr>
-            </table>
-            <div class="text">
-                <strong><?= __('Message') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($systemLog->message)); ?>
-                </blockquote>
-            </div>
-            <div class="text">
-                <strong><?= __('Context') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($systemLog->context)); ?>
-                </blockquote>
+<div class="container mt-5">
+    <div class="row">
+        <?php
+        echo $this->element('actions_card', [
+            'modelName' => 'SystemLog',
+            'controllerName' => 'SystemLogs',
+            'entity' => $systemLog,
+            'entityDisplayName' => $systemLog->group_name . ':' . $systemLog->level . ':' . $systemLog->created->format('Y-m-d H:i:s'),
+            'hideNew' => true,
+            'hideEdit' => true,
+        ]);
+        ?>
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header bg-<?= strtolower($systemLog->level) ?>">
+                    <h3 class="card-title text-white mb-0"><?= h($systemLog->level) ?></h3>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped">
+                        <tr>
+                            <th><?= __('Level') ?></th>
+                            <td><span class="badge bg-<?= strtolower($systemLog->level) ?>"><?= h($systemLog->level) ?></span></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Group Name') ?></th>
+                            <td><?= h($systemLog->group_name) ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Created') ?></th>
+                            <td><?= $systemLog->created->format('Y-m-d H:i:s') ?></td>
+                        </tr>
+                    </table>
+                    <div class="mt-4">
+                        <h5><?= __('Message') ?></h5>
+                        <div class="bg-light p-3 rounded">
+                            <?php
+                            $lines = explode("\n", h($systemLog->message));
+                            foreach ($lines as $line) {
+                                if (strpos($line, 'Stack Trace') !== false) {
+                                    echo "<strong class='text-danger'>$line</strong><br>";
+                                } elseif (preg_match('/^#\d+/', $line)) {
+                                    echo "<code class='text-muted'>$line</code><br>";
+                                } else {
+                                    echo "$line<br>";
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <?php if (!empty($systemLog->context)): ?>
+                    <div class="mt-4">
+                        <h5><?= __('Context') ?></h5>
+                        <pre class="bg-light p-3 rounded"><code><?= json_encode(json_decode($systemLog->context), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) ?></code></pre>
+                    </div>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>

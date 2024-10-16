0ac2dea115d45dd4f2848af1bd206e0851abd3d8
Matthew Deaves <mndeaves@me.com>
2024-10-17 00:44:44 +0100

    fix system log test

--- tests/TestCase/Controller/SystemLogsControllerTest.php
+++ tests/TestCase/Controller/SystemLogsControllerTest.php
@@ -58,7 +58,7 @@ class SystemLogsControllerTest extends AppControllerTestCase
         $this->post('/admin/system-logs/delete/550e8400-e29b-41d4-a716-446655440000');
 
         $this->assertRedirect(['action' => 'index']);
-        $this->assertFlashMessage('The system log has been deleted.');
+        $this->assertFlashMessage('The log has been deleted.');
     }
 
     public function testSearch(): void

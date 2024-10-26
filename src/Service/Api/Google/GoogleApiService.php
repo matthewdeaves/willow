d76afa3eeda19727cc49a13edcf8a93c26a2df67
Matthew Deaves <mndeaves@me.com>
2024-10-26 11:47:06 +0100

    docblocks

--- src/Service/Api/Google/GoogleApiService.php
+++ src/Service/Api/Google/GoogleApiService.php
@@ -6,10 +6,22 @@ namespace App\Service\Api\Google;
 use App\Utility\SettingsManager;
 use Google\Cloud\Translate\V2\TranslateClient;
 
+/**
+ * Service class for interacting with the Google Cloud Translate API.
+ */
 class GoogleApiService
 {
+    /**
+     * The Google Cloud Translate client instance.
+     *
+     * @var TranslateClient
+     */
     private TranslateClient $translateClient;
 
+    /**
+     * Constructor for the GoogleApiService class.
+     * Initializes the Google Cloud Translate client with the API key from the settings.
+     */
     public function __construct()
     {
         $this->translateClient = new TranslateClient([
@@ -17,9 +29,16 @@ class GoogleApiService
         ]);
     }
 
+    /**
+     * Translates an array of strings from one language to another using batch translation.
+     *
+     * @param array $strings The array of strings to be translated.
+     * @param string $localeFrom The source language locale code (e.g., 'en' for English).
+     * @param string $localeTo The target language locale code (e.g., 'fr' for French).
+     * @return array An array containing the translated strings and their original values.
+     */
     public function translateStrings(array $strings, string $localeFrom, string $localeTo): array
     {
-
         $results = $this->translateClient->translateBatch($strings, [
             'source' => $localeFrom,
             'target' => $localeTo,

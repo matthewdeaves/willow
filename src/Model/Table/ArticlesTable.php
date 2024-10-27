e70650862727578edaede828d3311de5bf511332
Matthew Deaves <mndeaves@me.com>
2024-10-27 08:31:37 +0000

    tags translate behavior added

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -111,10 +111,26 @@ class ArticlesTable extends Table
         ]);
 
         $this->addBehavior('Translate', [
-            'fields' => ['title', 'body', 'summary'],
+            'fields' => [
+                'title',
+                'body',
+                'summary',
+                'meta_title',
+                'meta_description',
+                'meta_keywords',
+                'facebook_description',
+                'linkedin_description',
+                'instagram_description',
+                'twitter_description',
+            ],
             'defaultLocale' => 'en_GB',
             'allowEmptyTranslations' => false,
         ]);
+
+
+        
+
+
 
         $this->addBehavior('Josegonzalez/Upload.Upload', [
             'image' => [

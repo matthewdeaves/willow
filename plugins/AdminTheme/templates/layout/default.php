52dba1c28dd1cf6897b34ac16e62b0deea0a295b
Matthew Deaves <mndeaves@me.com>
2024-11-14 18:06:53 +0000

    user actions widget

--- plugins/AdminTheme/templates/layout/default.php
+++ plugins/AdminTheme/templates/layout/default.php
@@ -212,33 +212,7 @@ use Cake\Core\Configure;
             </li>
           </ul>
           <div class="d-flex align-items-center">
-            <div class="flex-shrink-0 dropdown ms-auto">
-              <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <?php if (isset($profilePic)) : ?>
-                <?= $this->Html->image(
-                  SettingsManager::read('ImageSizes.teeny', '200') . '/' . $profilePic, [
-                    'pathPrefix' => 'files/Users/image/',
-                    'class' => 'rounded-circle',
-                    'width' => '32',
-                    'height' => '32',
-                  ]); ?>
-                <?php else: ?>
-                  <img src="/img/willow-icon.png" width="32" height="32" class="rounded-circle">
-                <?php endif; ?>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-end text-small shadow">
-                <li>
-                <?= $this->Html->link(__('Front Site'), '/', ['class' => 'dropdown-item']) ?>
-                </li>
-                <li>
-                <?= $this->Html->link(__('My Account'), ['prefix' => 'Admin', 'controller' => 'Users', 'action' => 'view', $this->Identity->get('id')], ['class' => 'dropdown-item']) ?>
-                </li>
-                <li><hr class="dropdown-divider"></li>
-                <li>
-                    <?= $this->Html->link(__('Logout'), ['prefix' => 'Admin', 'controller' => 'Users', 'action' => 'logout'], ['class' => 'dropdown-item']) ?>
-                </li>
-              </ul>
-            </div>
+            <?= $this->element('user_actions') ?>
           </div>
         </div>
       </div>

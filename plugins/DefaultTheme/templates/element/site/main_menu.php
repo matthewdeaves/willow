72997346c01f06d8dacc3dc84876ed180c97495e
Matthew Deaves <mndeaves@me.com>
2024-11-14 18:49:28 +0000

    highlight current page in main menu

--- plugins/DefaultTheme/templates/element/site/main_menu.php
+++ plugins/DefaultTheme/templates/element/site/main_menu.php
@@ -1,14 +1,22 @@
 <?php $mbAmount = $mbAmount ?? 0; ?>
+<?php $currentUrl = $this->request->getPath(); ?>
+
 <div class="nav-scroller py-1 mb-<?= $mbAmount ?> border-bottom">
     <nav class="nav nav-underline justify-content-center">
-        <?= $this->Html->link(__('Blog'), ['_name' => 'home'], ['class' => 'nav-item nav-link link-body-emphasis active']) ?>
+
+        <?php $url = $this->Html->Url->build(['_name' => 'home']); ?>
+        <?= $this->Html->link(__('Blog'), $url, [
+            'class' => 'nav-item nav-link link-body-emphasis' . (($currentUrl == $url) ? ' active' : '')
+        ]) ?>
+
         <?php foreach ($rootPages as $rootPage) : ?>
+            <?php $url = $this->Html->Url->build(['_name' => 'page-by-slug', 'slug' => $rootPage['slug']]); ?>
             <?=
                 $this->Html->link(
                     htmlspecialchars_decode($rootPage['title']),
-                    ['_name' => 'page-by-slug', 'slug' => $rootPage['slug']],
+                    $url,
                     [
-                        'class' => 'nav-item nav-link link-body-emphasis',
+                        'class' => 'nav-item nav-link link-body-emphasis' . (($currentUrl == $url) ? ' active' : ''),
                         'escape' => false
                     ]
                 );

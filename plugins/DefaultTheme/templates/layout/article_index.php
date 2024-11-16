93efbe7eebb64ccb93d18dc0213a5fc7145b131f
Matthew Deaves <mndeaves@me.com>
2024-11-16 13:26:08 +0000

    default site description

--- plugins/DefaultTheme/templates/layout/article_index.php
+++ plugins/DefaultTheme/templates/layout/article_index.php
@@ -49,7 +49,7 @@
 
             <div class="p-4 mb-3 bg-body-tertiary rounded">
               <h4 class="fst-italic"><?= __('About') ?></h4>
-              <p class="mb-0"><?= __("I'm building a content management system called Willow and I blog about related software development, features and how you can use it too.") ?></p>
+              <p class="mb-0"><?= __("Welcome to Willow, a content management system I'm building in the open. Here you'll find development updates, feature highlights, and guides on using Willow for your own sites.") ?></p>
             </div>
 
             <?= $this->element('site/articles_list', ['articles' => $featuredArticles, 'title' => __('Featured posts')]) ?>

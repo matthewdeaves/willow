c6d8a9a5d38a102cc5e454c237ab865223046b15
Matthew Deaves <mndeaves@me.com>
2024-12-03 13:50:22 +0000

    udpated the form for robots.txt to match theme

--- plugins/AdminTheme/templates/Admin/Robots/edit.php
+++ plugins/AdminTheme/templates/Admin/Robots/edit.php
@@ -4,28 +4,56 @@
  * @var string $robotsContent
  */
 ?>
-<div class="robots form content">
-    <?= $this->Form->create(null) ?>
-    <fieldset>
-        <legend><?= __('Edit Robots.txt') ?></legend>
-        <?= $this->Form->control('robotsContent', [
-            'type' => 'textarea',
-            'value' => $robotsContent,
-            'rows' => 20,
-            'class' => 'form-control font-monospace',
-            'label' => false
-        ]) ?>
-    </fieldset>
-    <div class="d-flex justify-content-between align-items-center mt-3">
-        <?= $this->Html->link(
-            __('Reset to Default'),
-            ['action' => 'reset'],
-            [
-                'class' => 'button button-outline',
-                'confirm' => __('Are you sure you want to reset to the default robots.txt content?')
-            ]
-        ) ?>
-        <?= $this->Form->button(__('Save Changes'), ['class' => 'button']) ?>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title"><?= __('Actions') ?></h5>
+                </div>
+                <div class="card-body">
+                    <div class="list-group">
+                        <?= $this->Html->link(
+                            __('Reset to Default'),
+                            ['action' => 'reset'],
+                            [
+                                'class' => 'list-group-item list-group-item-action',
+                                'confirm' => __('Are you sure you want to reset to the default robots.txt content?')
+                            ]
+                        ) ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-9">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title"><?= __('Edit Robots.txt') ?></h5>
+                </div>
+                <div class="card-body">
+                    <?= $this->Form->create(null, [
+                        'class' => 'needs-validation',
+                        'novalidate' => true
+                    ]) ?>
+                    <fieldset>
+                        <?= $this->Form->control('robotsContent', [
+                            'type' => 'textarea',
+                            'value' => $robotsContent,
+                            'rows' => 20,
+                            'class' => 'form-control font-monospace',
+                            'label' => false,
+                            'required' => true
+                        ]) ?>
+                    </fieldset>
+                    <div class="form-group mt-3">
+                        <?= $this->Form->button(__('Save Changes'), [
+                            'class' => 'btn btn-primary'
+                        ]) ?>
+                    </div>
+                    <?= $this->Form->end() ?>
+                </div>
+            </div>
+        </div>
     </div>
-    <?= $this->Form->end() ?>
 </div>

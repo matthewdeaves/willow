83413bae947cefb8822bea07d0c516e090ce613c
Matthew Deaves <mndeaves@me.com>
2024-12-10 21:27:32 +0000

    More updating tests

--- tests/TestCase/Controller/SlugsControllerTest.php
+++ tests/TestCase/Controller/SlugsControllerTest.php
@@ -1,81 +1,202 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Test\TestCase\Controller;
+namespace App\Test\TestCase\Controller\Admin;
 
+use App\Test\TestCase\AppControllerTestCase;
 use Cake\TestSuite\IntegrationTestTrait;
-use Cake\TestSuite\TestCase;
 
 /**
- * App\Controller\SlugsController Test Case
+ * App\Controller\Admin\SlugsController Test Case
  *
- * @uses \App\Controller\SlugsController
+ * @uses \App\Controller\Admin\SlugsController
  */
-class SlugsControllerTest extends TestCase
+class SlugsControllerTest extends AppControllerTestCase
 {
     use IntegrationTestTrait;
 
     /**
      * Fixtures
      *
-     * @var list<string>
+     * @var array<string>
      */
     protected array $fixtures = [
+        'app.Users',
+        'app.Articles',
         'app.Slugs',
+        'app.Tags',
     ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        
+        // Login as admin user
+        $adminId = '6509480c-e7e6-4e65-9c38-1423a8d09d0f';
+        $this->loginUser($adminId);
+    }
 
     /**
      * Test index method
      *
      * @return void
-     * @uses \App\Controller\SlugsController::index()
      */
     public function testIndex(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('/admin/slugs');
+        $this->assertResponseOk();
+        $this->assertResponseContains('Slugs');
+        
+        // Test model type filtering
+        $this->get('/admin/slugs?status=Articles');
+        $this->assertResponseOk();
+        
+        // Test search functionality
+        $this->get('/admin/slugs?search=article-one');
+        $this->assertResponseOk();
+        
+        // Test AJAX request
+        $this->configRequest([
+            'headers' => ['X-Requested-With' => 'XMLHttpRequest']
+        ]);
+        $this->get('/admin/slugs?search=article');
+        $this->assertResponseOk();
     }
 
     /**
      * Test view method
      *
      * @return void
-     * @uses \App\Controller\SlugsController::view()
      */
     public function testView(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('/admin/slugs/view/1e6c7b88-283d-43df-bfa3-fa33d4319f75');
+        $this->assertResponseOk();
+        $this->assertResponseContains('article-one');
     }
 
     /**
      * Test add method
      *
      * @return void
-     * @uses \App\Controller\SlugsController::add()
      */
     public function testAdd(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+        
+        // Test GET request
+        $this->get('/admin/slugs/add');
+        $this->assertResponseOk();
+
+        // Test POST request with valid data
+        $this->post('/admin/slugs/add', [
+            'model' => 'Articles',
+            'foreign_key' => '263a5364-a1bc-401c-9e44-49c23d066a0f',
+            'slug' => 'new-test-slug',
+        ]);
+        
+        $this->assertRedirect(['action' => 'index']);
+        $this->assertFlashMessage('The slug has been saved.');
+
+        // Test POST request with invalid data
+        $this->post('/admin/slugs/add', [
+            'model' => '',
+            'foreign_key' => '',
+            'slug' => '',
+        ]);
+        
+        $this->assertResponseOk(); // Form should re-render
+        $this->assertFlashMessage('The slug could not be saved. Please, try again.');
     }
 
     /**
      * Test edit method
      *
      * @return void
-     * @uses \App\Controller\SlugsController::edit()
      */
     public function testEdit(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+        
+        // Test GET request
+        $this->get('/admin/slugs/edit/1e6c7b88-283d-43df-bfa3-fa33d4319f75');
+        $this->assertResponseOk();
+
+        // Test POST request with valid data
+        $this->post('/admin/slugs/edit/1e6c7b88-283d-43df-bfa3-fa33d4319f75', [
+            'model' => 'Articles',
+            'foreign_key' => '263a5364-a1bc-401c-9e44-49c23d066a0f',
+            'slug' => 'updated-test-slug',
+        ]);
+        
+        $this->assertRedirect(['action' => 'index']);
+        $this->assertFlashMessage('The slug has been saved.');
+
+        // Test POST request with invalid data
+        $this->post('/admin/slugs/edit/1e6c7b88-283d-43df-bfa3-fa33d4319f75', [
+            'model' => '',
+            'foreign_key' => '',
+            'slug' => '',
+        ]);
+        
+        $this->assertResponseOk(); // Form should re-render
+
+        $this->assertResponseContains('The slug could not be saved. Please, try again.');
+
+        // Test with non-existent ID
+        $this->get('/admin/slugs/edit/non-existent-id');
+        $this->assertResponseError();
     }
 
     /**
      * Test delete method
      *
      * @return void
-     * @uses \App\Controller\SlugsController::delete()
      */
     public function testDelete(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+        
+        // Test successful delete
+        $this->delete('/admin/slugs/delete/1e6c7b88-283d-43df-bfa3-fa33d4319f75');
+        $this->assertRedirect();
+        $this->assertFlashMessage('The slug has been deleted.');
+
+        // Test delete with non-existent ID
+        $this->delete('/admin/slugs/delete/non-existent-id');
+        $this->assertResponseError();
+
+        // Test with GET request (should fail)
+        $this->get('/admin/slugs/delete/1e6c7b88-283d-43df-bfa3-fa33d4319f75');
+        $this->assertResponseError();
+    }
+
+    /**
+     * Test access control
+     *
+     * @return void
+     */
+    public function testAccessControl(): void
+    {
+        // Logout
+        $this->session(['Auth' => null]);
+
+        // Test access to various actions
+        $this->get('/admin/slugs');
+        $this->assertRedirect('/en');
+
+        $this->get('/admin/slugs/add');
+        $this->assertRedirect('/en');
+
+        $this->get('/admin/slugs/edit/1e6c7b88-283d-43df-bfa3-fa33d4319f75');
+        $this->assertRedirect('/en');
+
+        $this->delete('/admin/slugs/delete/1e6c7b88-283d-43df-bfa3-fa33d4319f75');
+        $this->assertRedirect('/en');
     }
 }

e019212249b87a9c543fd156b4da3f3817a28e93
mndeaves <matthewdeaves@users.noreply.github.com>
2024-12-14 11:37:39 +0000

    Fix issue with nginx log folder not present on firt run of setup dev environment script (#52)
    * create log folder on statup on host machine
    
    * make jenkins optional with --jenkins on setup script
    
    * readme update

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Jenkins container is optional
+USE_JENKINS=0
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -j|--jenkins) USE_JENKINS=1 ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 # Detect the operating system
 OS="$(uname)"
 
@@ -11,6 +23,11 @@ needs_sudo() {
         echo ""
     fi
 }
+
+# Create required directories
+echo "Creating required directories..."
+mkdir -p logs/nginx
+chmod 777 logs/nginx
 
 # Function to check if Docker containers are running
 check_docker_status() {
@@ -24,7 +41,13 @@ check_docker_status() {
 # Start Docker containers if they're not running
 start_docker_containers() {
     echo "Starting Docker containers..."
-    $(needs_sudo) docker compose up -d
+    if [ "$USE_JENKINS" -eq 1 ]; then
+        echo "Including Jenkins in startup..."
+        $(needs_sudo) docker compose up -d willowcms mysql phpmyadmin mailpit redis-commander jenkins
+    else
+        echo "Starting without Jenkins..."
+        $(needs_sudo) docker compose up -d willowcms mysql phpmyadmin mailpit redis-commander
+    fi
 }
 
 # Function to wait for MySQL

33f929ae48697b39426a2711b79e9bb416ab9224
Matthew Deaves <mndeaves@me.com>
2024-12-15 11:16:50 +0000

    save point

--- plugins/AdminTheme/templates/layout/default.php
+++ plugins/AdminTheme/templates/layout/default.php
@@ -13,7 +13,14 @@ $session = $this->request->getSession();
     <title><?= SettingsManager::read('SEO.siteName', 'Willow CMS') ?>: <?= $this->fetch('title') ?></title>
     <?= $this->Html->meta('icon') ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <?= $this->Html->css('AdminTheme.willow-admin') ?>
+    <?= $this->Html->css([
+      'AdminTheme.base',
+      'AdminTheme.theme',
+      'AdminTheme.markdown',
+      'AdminTheme.trumbowyg',
+      'AdminTheme.semantic-ui-dropdown',
+      'AdminTheme.images-grid',
+    ], ['block' => true]) ?>
     <?= $this->Html->script('AdminTheme.image-preview') ?>
     <?= $this->Html->script('willow-modal') ?>
     <?= $this->Html->script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js'); ?>

af6b80005389d438e59c83ccce493a13d4d1e8f5
Matthew Deaves <mndeaves@me.com>
2025-05-15 23:51:50 +0100

    minor base view update

--- plugins/AdminTheme/src/View/AppView.php
+++ plugins/AdminTheme/src/View/AppView.php
@@ -1,24 +1,42 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     3.0.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
 namespace App\View;
 
 use Cake\View\View;
 
+/**
+ * Application View
+ *
+ * Your application's default view class
+ *
+ * @link https://book.cakephp.org/4/en/views.html#the-app-view
+ */
 class AppView extends View
 {
     /**
      * Initialization hook method.
      *
-     * Use this method to add common initialization code like loading helpers.
+     * Use this method to add common initialization code like adding helpers.
      *
-     * e.g. `$this->loadHelper('Html');`
+     * e.g. `$this->addHelper('Html');`
      *
      * @return void
      */
     public function initialize(): void
     {
-        // Don't forget to call parent::initialize()
         parent::initialize();
 
         // Set Bootstrap-friendly form templates
@@ -39,7 +57,5 @@ class AppView extends View
             'inputGroupText' => '<span class="input-group-text">{{content}}</span>',
             'inputGroupTextError' => '<span class="input-group-text">{{content}}</span>',
         ]);
-
-        // Add any other view initialization code here
     }
 }

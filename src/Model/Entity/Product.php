039e83c3a3147fa4d564760e2b3f5390a7a740c5
Mike <garzarobm@gmail.com>
2025-07-08 11:49:22 -0500

    Add Products model, controller, entity, and seed for product management

---
+++ src/Model/Entity/Product.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Product Entity
+ *
+ * @property int $product_id
+ * @property string $name
+ * @property string|null $price_usd
+ * @property string|null $category_rating
+ * @property string|null $comments
+ */
+class Product extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected array $_accessible = [
+        'name' => true,
+        'price_usd' => true,
+        'category_rating' => true,
+        'comments' => true,
+    ];
+}

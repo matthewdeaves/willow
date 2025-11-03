df4a4bd13b0d56b2c13b775584f4851110264a96
Matthew Deaves <mndeaves@me.com>
2025-11-03 08:40:15 +0000

    Fix comment form routing issue in ArticleCommentsCell
    When comment form is rendered in a plugin template, CakePHP assumes the target
    controller is also in the plugin unless explicitly specified otherwise. This caused
    the form to look for DefaultTheme\ArticlesController instead of App\ArticlesController.
    
    Changes:
    - Add 'plugin' => false to comment form URL to route to main app controller
    - Add 'plugin' => false to login link URL for consistency
    - Fixes MissingControllerException when submitting comments
    
    Bug: Adding a comment threw error "DefaultTheme.ArticlesController could not be found"
    Fix: Explicitly specify plugin context in URL arrays

--- plugins/DefaultTheme/templates/cell/ArticleComments/display.php
+++ plugins/DefaultTheme/templates/cell/ArticleComments/display.php
@@ -24,7 +24,7 @@
 <?php if ($this->Identity->isLoggedIn()): ?>
     <section class="add-comment mb-4">
         <h3 class="mb-3"><?= __('Add a Comment') ?></h3>
-        <?= $this->Form->create(null, ['url' => ['controller' => 'Articles', 'action' => 'addComment', $article->id]]) ?>
+        <?= $this->Form->create(null, ['url' => ['plugin' => false, 'controller' => 'Articles', 'action' => 'addComment', $article->id]]) ?>
             <?= $this->Form->control('content', ['label' => __('Comment'), 'type' => 'textarea', 'rows' => 3, 'class' => 'form-control mb-3']) ?>
             <?= $this->Form->button(__('Submit Comment'), ['class' => 'btn btn-primary']) ?>
         <?= $this->Form->end() ?>
@@ -32,6 +32,6 @@
 <?php else: ?>
     <section class="login-prompt mb-4">
         <h3 class="mb-3"><?= __('Please log in to add a comment') ?></h3>
-        <?= $this->Html->link(__('Login'), ['controller' => 'Users', 'action' => 'login'], ['class' => 'btn btn-primary']) ?>
+        <?= $this->Html->link(__('Login'), ['plugin' => false, 'controller' => 'Users', 'action' => 'login'], ['class' => 'btn btn-primary']) ?>
     </section>
 <?php endif; ?>
